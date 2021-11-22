44acd867fc42795639db3687df7e786361e78692
Peje <guillepazerrobidart@yahoo.com.ar>
2021-11-22 18:18:55 -0300

    [MAKE FILES] heroku app

---
+++ app.js
@@ -0,0 +1,35 @@
+import methodOverride from 'method-override';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+const log = console.log;
+app.use(cors());
+app.use(methodOverride())
+
+let port = process.env.PORT || 3000;
+
+let users = ['bart', 'lisa', 'homero', 'marge', 'maggie'];
+
+app.get("/users", (req, res) =>{
+    res.send(users)
+});
+
+app.post("/user/create/:nombre", (req, res) => {
+    users.push(req.params.nombre);
+    res.send('usuario creado');
+});
+
+app.delete("/user/delete/:nombre", (req, res) => {
+    users = users.filter(el => el != req.params.nombre)
+    res.send("usuario eliminado")
+});
+
+app.put("/user/change/:nombre/:nombrenuevo", (req, res) => {
+    users = users.map(el => (req.params.nombre === el) ? req.params.nombrenuevo : el)
+    res.send("nombre cambiado")
+})
+
+app.listen(port, () =>{
+    log('start server')
+})
